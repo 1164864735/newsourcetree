e0998f7c7d02e43e664993aa6802e07a1005fc9b
Leon <526247082@qq.com>
2016-09-10 09:41:58 +0800

    Use butterknifte bind view.

--- app/src/main/java/com/itheima/sexygirl/MainActivity.java
+++ app/src/main/java/com/itheima/sexygirl/MainActivity.java
@@ -1,13 +1,23 @@
 package com.itheima.sexygirl;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.list_view)
+    ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
 }
